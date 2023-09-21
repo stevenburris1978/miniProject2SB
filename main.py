aa1f4387e4d7ece84bc0d3527cda55d83690b898
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-09-21 15:49:41 -0500

    made graph of top salaries and saved the graph to a file

--- main.py
+++ main.py
@@ -20,10 +20,32 @@ print(goodSalaries)
 bestSalaries = [i for i in goodSalaries if i > 100000]
 print(bestSalaries)
 
+days = list(range(1, len(bestSalaries) + 1))
+
+plt.plot(days, bestSalaries, 'red')
+plt.plot(days, bestSalaries, 'o')
+
+salaries = bestSalaries
+salaries.sort()
+lowPrice = salaries[0]
+highPrice = salaries[-1]
+
+plt.axis([1, 50, lowPrice - 1, highPrice + 1])
+
+plt.title('Best Software Salaries')
+plt.xlabel('Cities')
+plt.ylabel('Salary')
+
 # code to create charts folder if there is not one already
 try:
     Path("charts").mkdir()
 except FileExistsError:
     pass
+
+# auto save png pics to folder with stock name
+savefile = "charts/" + "chart1.png"
+plt.savefig(savefile)
+plt.show()
+
 
 
